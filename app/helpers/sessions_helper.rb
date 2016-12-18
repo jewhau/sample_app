e0890d2a4acda9d88fa7d60e3d1f261ff245bd96
jewhau <419782021@qq.com>
2016-12-18 09:17:50 +0000

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,6 +10,10 @@ module SessionsHelper
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
+  
+  def current_user?(user)
+    user == current_user
+  end
 
   # 返回当前登录的用户（如果有的话）
   def current_user
@@ -39,5 +43,15 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+  
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # 存储后面需要使用的地址
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
